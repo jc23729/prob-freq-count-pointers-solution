7de7f86fb37a25cc23343269c31d45c6c66854d7
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-11 13:55:14 -0400

    Update construct-note.js

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,21 @@
+// constructNote
+// Write a function called constructNote, which accepts two strings, a message and some letters. The function should return true 
+// if the message can be built with the letters that you are given; otherwise, it should return false.
+
+// Assume that there are only lowercase letters and no space or special characters in both the message and the letters.
+
+// Constraints:
+
+// Time Complexity: O(M + N) - If M is the length of message and N is the length of letters:
+
+// Examples:
+
+// constructNote('aa', 'abc') // false
+// constructNote('abc', 'dcba') // true
+// constructNote('aabbcc', 'bcabcaddff') // true
+
+
+
 /**
  * Build two frequency counters. If any of the message characters are not
  *  found in the 'letters' characters, or if there are not enough of them,
