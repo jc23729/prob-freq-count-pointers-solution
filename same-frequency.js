a66b8a056e2a531a0440fc798e818f446fbce6ff
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-11 20:13:44 -0400

    Update same-frequency.js

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,18 @@
+// sameFrequency
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Examples:
+
+// sameFrequency(182,281) // true
+// sameFrequency(34,14) // false
+// sameFrequency(3589578, 5879385) // true
+// sameFrequency(22,222) // false
+// Constraints
+
+// Time Complexity - O(N + M)
+
+
+
 function sameFrequency(num1, num2) {
   let str1 = num1.toString();
   let str2 = num2.toString();
